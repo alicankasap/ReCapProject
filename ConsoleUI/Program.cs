7aced7ae707b0895ebf49028ff7a61306b04c81d
alicankasap <alicankasap96@gmail.com>
2022-06-21 22:01:02 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            //carManager.Add(new Car { BrandId = 3, ColorId = 3, ModelYear = 2009, DailyPrice = 5000, Description = "BMW 330d Automatic" });
+
+            carManager.Update(new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2009, DailyPrice = 500, Description = "BMW 330d Automatic" });
+
+            //carManager.Delete(new Car { Id = 2 });
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"Id: {car.Id}   Model: {car.Description}\n");
+            }
+
+            //foreach (var car in carManager.GetCarsByBrandId(1))
+            //{
+            //    Console.WriteLine($"Id: {car.Id}   Model: {car.Description}\n");
+            //}
+
+            //foreach (var car in carManager.GetCarsByColorId(2))
+            //{
+            //    Console.WriteLine($"Id: {car.Id}   Model: {car.Description}\n");
+            //}
+
+            //Console.WriteLine(carManager.GetById(3).Description);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 250, ModelYear = 2015, Description = "Seat Ibiza 1.2 TSI Manuel" },
+                new Car { Id = 2, BrandId = 2, ColorId = 2, DailyPrice = 350, ModelYear = 2015, Description = "Toyota Corolla 1.6 Benzin Automatic" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById(int id)
+        {
+            Car car = _cars.SingleOrDefault(c => c.Id == id);
+
+            return car;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
