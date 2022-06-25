1c969c683299b91bd2ccaec4766a8622b970d23e
alicankasap <alicankasap96@gmail.com>
2022-06-25 22:25:22 +0300

    9.Gün Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,17 +11,28 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
 
-            //carManager.Add(new Car { BrandId = 3, ColorId = 3, ModelYear = 2009, DailyPrice = 5000, Description = "BMW 330d Automatic" });
+            //brandManager.Update(new Brand { Id = 1002, Name = "Volvo" });
 
-            carManager.Update(new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2009, DailyPrice = 500, Description = "BMW 330d Automatic" });
+            //colorManager.Add(new Color { Name = "Siyah" });
+
+            //carManager.Add(new Car { BrandId = 1002, ColorId = 1002, ModelYear = 2022, DailyPrice = 1000, Description = "XC40 T3 Automatic" });
+
+            //carManager.Update(new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2009, DailyPrice = 500, Description = "BMW 330d Automatic" });
 
             //carManager.Delete(new Car { Id = 2 });
 
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine($"Id: {car.Id}   Model: {car.Description}\n");
-            }
+            //foreach (var car in carManager.GetCarDetails())
+            //{
+            //    Console.WriteLine(car.CarName + " " + car.BrandName + " " + car.ColorName + " " + car.DailyPrice);
+            //}
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine($"Id: {car.Id}   Model: {car.Description}\n");
+            //}
 
             //foreach (var car in carManager.GetCarsByBrandId(1))
             //{
